d4ce7754e0673fb1dc3326292012bd8cafcb9ba0
= <=>
2023-08-14 00:01:51 +0530

    initial commit

---
+++ all about project.py
@@ -0,0 +1,16 @@
+Language : Python
+module :
+ 		tkinter		-> to design desktop base application
+ 		pysql 		-> for data base
+
+ 		json		-> read json file
+
+ 		editor : sublime , notepad , vs code, spider
+
+Hard :
+		RAM : 4gb or above
+		HDD : 80GB or above		
+
+->2 module use in this project
+1)User
+2)Host		
---
+++ relational.java
@@ -0,0 +1,16 @@
+class Test
+{
+	public static void main(String[] args) {
+		System.out.println( 50 == 30);
+		System.out.println( 'n' == 97);
+		System.out.println( 'a' == 97);
+		System.out.println( 98.0 == 'b');
+		System.out.println( true == false);
+
+		System.out.println( 50 < 20);
+		System.out.println( 'a' > 97.1);
+		System.out.println( 'b' > 'a');
+		System.out.println( true > false);
+	}
+		
+}
